fb5cfce70ce797e83b4ead2b98dac6f98d352364
ShubhT1575 <st9889394838@gmail.com>
2024-04-29 15:01:39 +0530

    fixed bug

--- frontend/src/Components/miscellaneous/ProfileModal.js
+++ frontend/src/Components/miscellaneous/ProfileModal.js
@@ -2,6 +2,8 @@ import {
   Avatar,
   Box,
   Button,
+  IconButton,
+  Image,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -9,35 +11,54 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Text,
   useDisclosure,
 } from "@chakra-ui/react";
+import { ViewIcon } from "@chakra-ui/icons"
 import React from "react";
 
 const ProfileModal = ({ user, children }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
-      <span onClick={onOpen}>{children}</span>
-      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      {children ? (
+        <span onClick={onOpen}>{children}</span>
+      ) : (
+        <IconButton d={{ base: "flex" }} icon={<ViewIcon />} onClick={onOpen} />
+      )}
+      <Modal size="lg" onClose={onClose} isOpen={isOpen} isCentered>
         <ModalOverlay />
-        <ModalContent>
-          <ModalHeader className="modal modal-heading" fontWeight="bold">
+        <ModalContent h="410px">
+          <ModalHeader
+            fontSize="40px"
+            fontFamily="Work sans"
+            display="flex"
+            justifyContent="center"
+          >
             {user.name}
           </ModalHeader>
-
-          <ModalBody className="modal">
-            <Avatar
-              size="2xl"
-              cursor="pointer"
-              name={user.name}
+          <ModalCloseButton />
+          <ModalBody
+            display="flex"
+            flexDir="column"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Image
+              borderRadius="full"
+              boxSize="150px"
               src={user.pic}
+              alt={user.name}
             />
-            Email : {user.email}
+            <Text
+              fontSize={{ base: "28px", md: "30px" }}
+              fontFamily="Work sans"
+            >
+              Email: {user.email}
+            </Text>
           </ModalBody>
           <ModalFooter>
-            <Button colorScheme="purple" mr={3} onClick={onClose}>
-              Close
-            </Button>
+            <Button colorScheme="purple" onClick={onClose}>Close</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
